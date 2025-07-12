94af8d7e360d6ea0851414da4d47f7affd05fc50
HaekalAlif <haekalalifputra@gmail.com>
2025-07-13 02:50:14 +0700

    feat: finalize Product section and complete Home page layout

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/features/landing/Hero.jsx";
 import SpecialOffer from "../../components/features/landing/SpecialOffer.jsx";
+import ProductSection from "../../components/features/landing/Product.jsx";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <SpecialOffer />
+      <ProductSection />
     </>
   );
 };
